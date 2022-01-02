e0baa6d593246899f89a5b9e358db5d6bb69d148
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-01-02 12:28:18 +0530

    initial commit

---
+++ src/Accordion.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Accordion = ({heading, desc}) => {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+       <>
+          <div className="flex flex-col items-center justify-center mt-8">
+           <div onClick={() => setIsActive(!isActive)} className="flex justify-between max-w-sm bg-slate-500 p-4 w-96 cursor-pointer font-semibold text-xl">
+               {heading}
+               <span className="font-semibold text-2xl"> {isActive ? "-" : "+"} </span>
+           </div>
+
+            {
+                isActive ? (
+                    <div className="p-4 bg-zinc-400 w-96 font-semibold text-xl">
+                        {desc}
+                    </div>
+                ) : ""
+            }
+
+          </div>  
+       </>
+              
+      
+    )
+}
+
+export default Accordion
--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import Accordion from './Accordion';
 import './App.css';
+
+const dataObj = [
+  {
+    "heading": "Section one",
+    "desc": "This is a description of Section one"
+  },
+  {
+    "heading": "Section two",
+    "desc": "This is a description of Section two"
+  },
+  {
+    "heading": "Section three",
+    "desc": "This is a description of Section three"
+  },
+  {
+    "heading": "Section four",
+    "desc": "This is a description of Section four"
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-4 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-semibold"> Accordion </h1>
+      <hr className='w-48 border-2 border-red-900' />
+      {
+        dataObj.map(({heading, desc}) => (
+          <Accordion heading={heading} desc={desc} />
+        ))
+      }
     </div>
   );
 }
